c8fef47a80d459a6ff99055db9880e558c82f326
Ranjithkumar7200 <ranjithkumarb8072@gmail.com>
2023-10-18 13:40:48 +0530

    added react table

--- src/Components/pages/UserReport.js
+++ src/Components/pages/UserReport.js
@@ -1,9 +1,19 @@
 import React, { useMemo, useState } from 'react'
 import { Button, Col, Container, Form, Row, Table } from 'react-bootstrap'
-
+import {useTable,useSortBy,usePagination} from 'react-table'
+import MOCK_DATA from '../pages/Table/MOCK_DATA.json'
+import {COLUMNS} from '../pages/Table/Columns'
 
 const UserReport = () => {
+  const columns = useMemo(()=>COLUMNS,[]);
+  const data = useMemo(()=>MOCK_DATA,[]);
+   const {getTableProps,getTableBodyProps,headerGroups,rows,prepareRow,page,nextPage,previousPage,canNextPage,canPreviousPage,pageOptions,state} = useTable({
+    columns,
+    data
+  },
+  useSortBy,usePagination)
   
+  const {pageIndex}= state;
   return (
     <>
     <Container fluid className='d-flex flex-column'>
@@ -38,7 +48,54 @@ const UserReport = () => {
         </Form>
       </Row>
       <Row>
-        
+        <Table {...getTableProps()} responsive={true}>
+          <thead>
+            {
+              headerGroups.map((headerGroups)=>(
+                <tr {...headerGroups.getHeaderGroupProps()}>
+                  {
+                    headerGroups.headers.map((column)=>(
+                      <th  {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                  {column.render('Header')}
+ 
+                               {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
+</th>
+                    ))
+                  }
+              </tr>
+              ))
+            }
+           
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            
+              {
+                page.map(row => {
+                  prepareRow(row)
+                  return(
+                    <tr {...row.getRowProps()}>
+                      {
+                        row.cells.map((cell)=>{
+                          return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                        })
+                      }
+                    </tr>
+                  )
+                })
+              }
+             
+          </tbody>
+        </Table>
+        <Col>
+        <span>
+          page 
+          <strong className='m-2'>
+            {pageIndex+1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <Button onClick={()=>previousPage()} disabled={!canPreviousPage} className='m-2'>previous</Button>
+        <Button onClick={()=>nextPage()} disabled={!canNextPage}>Next</Button>
+        </Col>
       </Row>
     </Container>
     </>
