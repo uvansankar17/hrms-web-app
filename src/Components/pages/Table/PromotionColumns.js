679145c22185040d99d1cdfbb2a6ebb37c691be2
Ranjithkumar7200 <ranjithkumarb8072@gmail.com>
2023-10-18 18:55:37 +0530

    adding promotion columns

---
+++ src/Components/pages/Table/PromotionColumns.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export const PromotionColumns = [
+    {
+        Header:"ID",
+        accessor:"id"
+    },
+    {
+        Header:"PROMOTED EMPLOYEE",
+        accessor:"promoted_employee"
+    },
+    {
+        Header:"DEPARTMENT",
+        accessor:"department"
+    },
+    {
+        Header:"PROMOTION FROM",
+        accessor:"promotion_from"
+    },
+    {
+        Header:"PROMOTION TO",
+        accessor:"promotion_to"
+    },
+    {
+        Header:"PROMOTION DATE",
+        accessor:"promotion_date"
+    },
+    
+    
+]
+
+
