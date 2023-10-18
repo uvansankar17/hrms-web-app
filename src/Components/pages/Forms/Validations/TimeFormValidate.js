35d6663a00aa6260085a65274b6fad9e6c8f6f42
Ranjithkumar7200 <134298773+Ranjithkumar7200@users.noreply.github.com>
2023-10-18 11:50:58 +0530

    Changing deadline to deadLine

--- src/Components/pages/Forms/Validations/TimeFormValidate.js
+++ src/Components/pages/Forms/Validations/TimeFormValidate.js
@@ -4,7 +4,7 @@ export const projectShema = Yup.object().shape({
     projectName: Yup.string()
     .min(3, 'Project Name should be at least 3 characters')
     .required('Project Name is required'),
-  deadline: Yup.date().required('Deadline is required'),
+  deadLine: Yup.date().required('Deadline is required'),
   totalHours: Yup.string().required('Total Hours is required'),
   remainingHours: Yup.string().required('Remaining Hours is required'),
   date: Yup.date().required('Date is required'),
