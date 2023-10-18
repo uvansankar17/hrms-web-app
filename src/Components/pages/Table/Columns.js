de954325f93a96ddda51bdac259352362321bdd1
Ranjithkumar7200 <ranjithkumarb8072@gmail.com>
2023-10-18 12:28:06 +0530

    adding data into column

---
+++ src/Components/pages/Table/Columns.js
@@ -0,0 +1,35 @@
+export const COLUMNS = [
+    {
+        Header:"ID",
+        accessor:"id"
+    },
+    {
+        Header:"FIRST NAME",
+        accessor:"first_name"
+    },
+    {
+        Header:"LAST NAME",
+        accessor:"last_name"
+    },
+    {
+        Header:"EMAIL",
+        accessor:"email"
+    },
+    {
+        Header:"GENDER",
+        accessor:"gender"
+    },
+    {
+        Header:"IP ADDRESS",
+        accessor:"ip_address"
+    },
+    {
+        Header:"PHONE",
+        accessor:"phone"
+    },
+    {
+        Header:"DATE",
+        accessor:"date"
+    }
+   
+]
